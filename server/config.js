ae75b0ececf0a7998f408883dbc8ac5aa572c251
artur.viachorka <artur.viachorka@itechart-group.com>
2017-09-20 15:19:48 +0300

    some style changing

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
     spotifyData: {
         CLIENT_ID: '83b59964e5284c788e0a04480b4122c5',
         CLIENT_SECRET: '9d1691cfd7d24ba4b2fa70defaf4949d',
-       	REDIRECT_URI: 'https://spotify-searcher.herokuapp.com/callback'
+       	REDIRECT_URI: 'http://localhost:3000/callback'
     },
     port: 3000
 };
