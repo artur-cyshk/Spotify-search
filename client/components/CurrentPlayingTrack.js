4de23375f9783f17da2a753bd235b7c68a1c5451
artur.viachorka <artur.viachorka@itechart-group.com>
2017-10-02 13:04:30 +0300

    some fixes for current playing

--- client/components/CurrentPlayingTrack.js
+++ client/components/CurrentPlayingTrack.js
@@ -7,12 +7,16 @@ import '../styles/currentPlaying.less';
 export default class CurrentPlayingTrack extends Component {
 
     componentDidMount() {
-        this.props.getDevices();
-    	this.props.getCurrentPlayingTrack();
+        this.loadCurrentPlayingTrack();
     }
 
     componentWillUnmount() {
         this.props.removeDevices();
+    }
+
+    loadCurrentPlayingTrack = () => {
+        this.props.getDevices();
+        this.props.getCurrentPlayingTrack();
     }
 
     nextTrack = () => {
@@ -23,63 +27,69 @@ export default class CurrentPlayingTrack extends Component {
         this.props.playTrack({device_id: this.activeDevice.id});
     }
 
+    getDeviceIcon = (deviceType) => {
+        switch (deviceType) {
+            case COMMON_DATA.deviceTypes.mobile:
+                return 'fa-mobile';
+            case COMMON_DATA.deviceTypes.pc:
+                return 'fa-desktop';
+            default:
+                return 'fa-ban';        
+        }
+    }
+
     render() {
         const { track, loadingCurrentPlayingTrack, loadingNextTrack, loadingPrevTrack, error } = this.props.currentPlayingTrack;
         const { loading: loadingDevices, list: devicesList } = this.props.devices;
         const album = track.album || {};
         const { images } = album;
         const artists = (track.artists || []).map((artist) => artist.name).join();
-
         const { product } = this.props.auth.user;
         const isPremium = product === COMMON_DATA.premiumAccount;
-        
         this.activeDevice = devicesList.find(device => device.is_active) || {};
-
+        const deviceIconClass = this.getDeviceIcon(this.activeDevice.type);
+        
         return (
             <div className="current-playing">
                 <header>Now playing </header>
                 <div>
                     <div className="player-control">
                         {isPremium && <button disabled={loadingPrevTrack} className="load-track load-prev-track" onClick={this.prevTrack}>
-                            {!loadingPrevTrack && <i className="fa fa-step-backward"></i>}
-                            {loadingPrevTrack && <Spinner/>}
+                            {loadingPrevTrack && <Spinner/> || <i className="fa fa-step-backward"></i>}
                         </button>}
                         <div className="track-info-wrapper">
                             <div className="active-device-type">
-                                {loadingDevices && <Spinner/>}
-                                {this.activeDevice.type === COMMON_DATA.deviceTypes.mobile && <i className="fa fa-mobile" aria-hidden="true"></i>}
-                                {this.activeDevice.type === COMMON_DATA.deviceTypes.pc && <i className="fa fa-desktop" aria-hidden="true"></i>}
+                                 {loadingDevices ? <Spinner/> : <i className={` fa ${deviceIconClass}`} aria-hidden="true"></i>}
                             </div>
-                            {loadingCurrentPlayingTrack && <Spinner/>}
-
-                            { !!Object.keys(error).length && 
-                                <span className="warning" hidden={!!track.name}>
+                            <i className="fa fa-refresh" onClick={this.loadCurrentPlayingTrack}/>
+                            {loadingCurrentPlayingTrack ? <Spinner/> :
+                                track.name ?
+                                <div className="track-info">
+                                    <div className="album-image">
+                                        <img src={!!images ? images[0].url : null}/>
+                                    </div>
+                                    <div className="song-info">
+                                        <span>
+                                            <i className="fa fa-book" aria-hidden="true"></i>
+                                            {album.name}
+                                        </span>
+                                        <span>
+                                            <i className="fa fa-users" aria-hidden="true"></i>
+                                            {artists}
+                                        </span>
+                                        <span>
+                                           <i className="fa fa-music" aria-hidden="true"></i>
+                                           {track.name}
+                                        </span>
+                                    </div>
+                                </div> :
+                                <span className="warning">
                                     { error.message ? error.message : 'There are no playing tracks' }
                                 </span>
                             }
-                            <div className="track-info" hidden={!track.name}>
-                                <div className="album-image">
-                                    <img src={!!images ? images[0].url : null}/>
-                                </div>
-                                <div className="song-info">
-                                    <span>
-                                        <i className="fa fa-book" aria-hidden="true"></i>
-                                        {album.name}
-                                    </span>
-                                    <span>
-                                        <i className="fa fa-users" aria-hidden="true"></i>
-                                        {artists}
-                                    </span>
-                                    <span>
-                                       <i className="fa fa-music" aria-hidden="true"></i>
-                                       {track.name}
-                                    </span>
-                                </div>
-                            </div>  
                         </div>
                         {isPremium && <button disabled={loadingNextTrack} className="load-track load-next-track" onClick={this.nextTrack}>
-                            {!loadingNextTrack && <i className="fa fa-step-forward"></i>}
-                            {loadingNextTrack && <Spinner/>}
+                            {loadingNextTrack ? <Spinner/> : <i className="fa fa-step-forward"></i>}
                         </button>}
                     </div>
 
